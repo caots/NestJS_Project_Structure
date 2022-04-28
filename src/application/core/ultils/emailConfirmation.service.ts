117101a4c05bb81294f4ce9cf1b3bfed900ca019
hungnm <hung.nguyenmanh@powergatesoftware.net>
2022-04-28 09:49:04 +0700

    update sendmail

--- src/application/core/ultils/emailConfirmation.service.ts
+++ src/application/core/ultils/emailConfirmation.service.ts
@@ -1,31 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import VerificationTokenPayload from '../interface/verificationTokenPayload.interface';
+
 import EmailService from './email.service';
+
  
 @Injectable()
 export class EmailConfirmationService {
   constructor(
-    private readonly jwtService: JwtService,
-    private readonly configService: ConfigService,
     private readonly emailService: EmailService,
   ) {}
  
-  public sendVerificationLink(email: string) {
-    const payload: VerificationTokenPayload = { email };
-    const token = this.jwtService.sign(payload, {
-      secret: this.configService.get('JWT_VERIFICATION_TOKEN_SECRET'),
-      expiresIn: `${this.configService.get('JWT_VERIFICATION_TOKEN_EXPIRATION_TIME')}s`
-    });
- 
-    const url = `${this.configService.get('EMAIL_CONFIRMATION_URL')}?token=${token}`;
- 
-    const text = `Welcome to the application. To confirm the email address, click here: ${url}`;
- 
+  public sendVerificationLink(email: string, subject: string, text: string) {
     return this.emailService.sendMail({
       to: email,
-      subject: 'Email confirmation',
+      subject,
       text,
     })
   }
--- src/application/events-handlers/users/sendMailConfirmEmail.handler.ts
+++ src/application/events-handlers/users/sendMailConfirmEmail.handler.ts
@@ -1,15 +1,30 @@
+import { ConfigService } from '@nestjs/config';
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { JwtService } from '@nestjs/jwt';
+import VerificationTokenPayload from 'src/application/core/interface/verificationTokenPayload.interface';
 import { EmailConfirmationService } from 'src/application/core/ultils/emailConfirmation.service';
 import { SendEmailConfirmEvent } from 'src/domain/events/users/send-mail-confirm-email';
 
 @EventsHandler(SendEmailConfirmEvent)
 export class SendEmailConfirmHandler implements IEventHandler<SendEmailConfirmEvent> {
-  
+
   constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
     private readonly emailConfirmationService: EmailConfirmationService
   ) { }
 
   async handle(event: SendEmailConfirmEvent) {
-    await this.emailConfirmationService.sendVerificationLink(event.email);
+    const payload: VerificationTokenPayload = { email: event.email };
+    const token = this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_VERIFICATION_TOKEN_SECRET'),
+      expiresIn: `${this.configService.get('JWT_VERIFICATION_TOKEN_EXPIRATION_TIME')}s`
+    });
+
+    const url = `${this.configService.get('EMAIL_CONFIRMATION_URL')}?token=${token}`;
+
+    const text = `Welcome to the application. To confirm the email address, click here: ${url}`;
+    const subject = "";
+    await this.emailConfirmationService.sendVerificationLink(event.email, subject, text);
   }
 }
